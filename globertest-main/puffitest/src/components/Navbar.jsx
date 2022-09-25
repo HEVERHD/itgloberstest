6e68cf095a6646927589ee22e2c365634a6651a8
unknown <hever11.hdgd@gmail.com>
2022-09-25 12:12:13 -0500

    contiditionals

--- globertest-main/puffitest/src/components/Navbar.jsx
+++ globertest-main/puffitest/src/components/Navbar.jsx
@@ -5,13 +5,19 @@ import puffinap from "../assets/img_nav/puffi_nap.png";
 import puffiPuff from "../assets/img_nav/puffi_puff.png";
 
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useMemo } from "react";
+import { startLogout } from "../store/auth";
 
 export const Navbar = () => {
   const { displayName, status } = useSelector((state) => state.auth);
-
   const authenticated = useMemo(() => status === "authenticated", [status]);
+
+  const dispatch = useDispatch();
+
+  const onLogout = () => {
+    dispatch(startLogout());
+  };
 
   return (
     <div className=" navbar_container ">
@@ -55,19 +61,35 @@ export const Navbar = () => {
           </Link>
         )}
 
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="12"
-          height="12"
-          fill="currentColor"
-          className="bi bi-chevron-down"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fillRule="evenodd"
-            d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-          />
-        </svg>
+        {status === "authenticated" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-escape"
+            viewBox="0 0 16 16"
+            onClick={onLogout}
+          >
+            <path d="M8.538 1.02a.5.5 0 1 0-.076.998 6 6 0 1 1-6.445 6.444.5.5 0 0 0-.997.076A7 7 0 1 0 8.538 1.02Z" />
+            <path d="M7.096 7.828a.5.5 0 0 0 .707-.707L2.707 2.025h2.768a.5.5 0 1 0 0-1H1.5a.5.5 0 0 0-.5.5V5.5a.5.5 0 0 0 1 0V2.732l5.096 5.096Z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="12"
+            height="12"
+            fill="currentColor"
+            className="bi bi-chevron-down"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+            />
+          </svg>
+        )}
+
         <span className="line_vertical"> </span>
         <button className="button">MI COMPRA</button>
       </div>
